90b89a22854292e48acb6079f5d7737e71b4c0b0
luisalfonsopreciado <luisapreciado99@gmail.com>
2020-11-26 21:07:55 -0600

    bfs

---
+++ graphs/traversal/BreadthFirstSearch.java
@@ -0,0 +1,49 @@
+package graphs.traversal;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+
+import graphs.Graph;
+
+public class BreadthFirstSearch {
+
+    Graph<Integer> g;
+
+    public BreadthFirstSearch() {
+        g = new Graph<>();
+    }
+
+    public void bfs(int start) {
+        Queue<Integer> queue = new LinkedList<>();
+        HashSet<Integer> visited = new HashSet<>();
+
+        queue.add(start);
+        visited.add(start);
+
+        while (!queue.isEmpty()) {
+            Integer curr = queue.remove();
+            System.out.print(curr + " ");
+
+            for (int adj : g.adj.get(curr)) {
+                if (visited.contains(adj))
+                    continue;
+                visited.add(adj);
+                queue.add(adj);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        BreadthFirstSearch dfs = new BreadthFirstSearch();
+        dfs.g.addUndirectedEdge(5, 2);
+        dfs.g.addUndirectedEdge(5, 0);
+        dfs.g.addUndirectedEdge(4, 0);
+        dfs.g.addUndirectedEdge(4, 1);
+        dfs.g.addUndirectedEdge(2, 3);
+        dfs.g.addUndirectedEdge(3, 1);
+
+        System.out.println("Following is a DFS " + "of the given graph");
+        dfs.bfs(5);
+    }
+}
